0a722ac6ab66f6a453220f50edd9cde631942410
maserari <mopihataiko@inbox.ru>
2021-09-15 23:56:12 +0300

    second

--- bsuLab1/bsuLab1.cpp
+++ bsuLab1/bsuLab1.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-	cout << "fOK";
-}
+	double x, e;
+	cin >> x >> e;
+	double result = 1;
+	double member = 1;
+	for (int i = 1; fabs(member) >= e; i++)
+	{
+		member *= x * x;
+		member /= 2 * i * (2 * i - 1);
+		result += member;
+	}
+	cout << result;
 
+
+	return 0;
+}
